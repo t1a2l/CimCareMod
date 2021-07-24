2556f1b00914a4fc01114650a0c56b0aa1cd0c37
Tal <talrofeh@gmail.com>
2021-07-24 23:01:29 +0300

    support old saves

---
+++ SeniorCitizenCenterMod/SeniorCitizenCenterMod/BackwardСompatibility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SeniorCitizenCenterMod
+{
+    public class BackwardСompatibilityBinder : SerializationBinder
+    {
+        public override Type BindToType(string assemblyName, string typeName)
+        {
+            if (assemblyName == "SeniorCitizenCenterMod")
+            {
+                switch (typeName)
+                {
+                    case "SeniorCitizenCenterMod.NursingHomeAi": return typeof(NursingHomeAI);
+                }
+            }
+
+            var type = Type.GetType($"{typeName}, {assemblyName}");
+            return type;
+        }
+    }
+}
---
+++ SeniorCitizenCenterMod/SeniorCitizenCenterMod/OldAssetsCompatibility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using HarmonyLib;
+
+namespace SeniorCitizenCenterMod
+{
+    [HarmonyPatch(typeof(PackageHelper), "ResolveLegacyTypeHandler")]
+    static class OldAssetsCompatibility
+    {
+        // 'SeniorCitizenCenterMod.NursingHomeAi, SeniorCitizenCenterMod, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null'
+        [HarmonyPostfix]
+        public static void Postfix(ref string __result)
+	    {
+            string[] temp = __result.Split(',');
+            if(temp[1] == " SeniorCitizenCenterMod")
+            {
+                if(temp[0] == "SeniorCitizenCenterMod.NursingHomeAi")
+                {
+                    __result = "SeniorCitizenCenterMod.NursingHomeAI, SeniorCitizenCenterMod, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+                }
+            }
+		    Logger.logInfo(__result);
+	    }
+    }
+}
